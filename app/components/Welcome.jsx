fdc6c5d5f402f0090f9968aba7de8b9487c0ed7e
CK-codemax <ckcodemax@gmail.com>
2024-02-26 03:50:31 +0100

    trying newer methods with auth

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -1,13 +1,17 @@
 'use client'
 
 import { useSession } from "next-auth/react"
+import Link from "next/link"
 import { redirect } from "next/navigation"
 
 export default function Welcome() {
     const {data : session} = useSession()
 
-    if(!session)redirect('/api/auth/signin/google')
+    
     return (
-      <div>let us see new facebook</div>
+      <div>
+        <p>welcome to my facebook clone</p>
+        <Link href={'/api/auth/signin/google'}>Click here to view more</Link>
+      </div>
     )
 }
