a9e7785be59c2e342a0032c00b6ddef44d67bcb4
CK-codemax <ckcodemax@gmail.com>
2024-03-01 00:18:08 +0100

    I disabled the input form in the signin page, allowing users to signin with just google oauth

--- app/components/SignIn.jsx
+++ app/components/SignIn.jsx
@@ -18,15 +18,15 @@ export default function SignIn() {
     <form className="flex flex-col space-y-3 w-[85%] sm:w-[450px] items-center justify-center">
        <div className="flex flex-col w-full items-start justify-start space-y-1">
            <label className="font-semibold text-left" htmlFor="email">Your email</label>
-           <input className="border border-gray-700 w-full p-2 px-4 rounded-md text-black placeholder:text-gray-500 outline-blue-500" id="email" placeholder="Enter your email" type="text" />
+           <input disabled className="border border-gray-700 w-full p-2 px-4 rounded-md text-black placeholder:text-gray-500 outline-blue-500" id="email" placeholder="Enter your email" type="text" />
        </div>
 
        <div className="flex flex-col items-start justify-start  space-y-1 w-full">
            <label className="font-semibold text-left" htmlFor="email">Your password</label>
-           <input className="border border-gray-700 w-full p-2 px-4 rounded-md text-black placeholder:text-gray-500 outline-blue-500" id="email" placeholder="Enter your password" type="text" />
+           <input disabled className="border border-gray-700 w-full p-2 px-4 rounded-md text-black placeholder:text-gray-500 outline-blue-500" id="email" placeholder="Enter your password" type="text" />
        </div>
 
-        <button className="w-full p-2 px-4 bg-blue-500 rounded-md text-gray-100 hover:font-semibold  hover:text-white xl:hover:scale-105 transition-all duration-300 ease-in-out">Login</button>
+        <button disabled className="w-full p-2 cursor-not-allowed px-4 bg-blue-500 rounded-md text-gray-100 hover:font-semibold  hover:text-white xl:hover:scale-105 transition-all duration-300 ease-in-out">Login</button>
     </form>
 
     <button className="w-[85%] sm:w-[450px] p-2 px-4 rounded-md justify-center text-gray-800 flex space-x-2 border-gray-800 border hover:bg-gray-200 hover:border-black bg-transparent hover:text-black xl:hover:scale-105 transition-all duration-300 ease-in-out" onClick={() => signIn('google')}>
